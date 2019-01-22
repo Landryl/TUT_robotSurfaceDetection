dfa18c1bd4854750475a1f1211181b38bc90cb38
Millet <arthur.millet@student.tut.fi>
2019-01-22 17:32:15 +0200

    test file

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+### Test file
+
 if __name__ == "__main__":
     
-    exec(open("classification.py").read())
+    nb_test = 2
+    
+    for i in range(0,nb_test):
+        print('test number :',i)
+        exec(open("classification.py").read())
