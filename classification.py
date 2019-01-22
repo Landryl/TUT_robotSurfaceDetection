e81d26e0f76904e5ba9ff5fa153efc0ada868a5d
Mederic <carriatmederic@gmail.com>
2019-01-22 12:58:11 +0200

    Add other classifiers
    Signed-off-by: Mederic <carriatmederic@gmail.com>

--- classification.py
+++ classification.py
@@ -12,15 +12,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
 # Importing the dataset
 dataset = pd.read_csv('./dataset/y_train_final_kaggle.csv')
 X_raw = np.load("dataset/X_train_kaggle.npy")
 y = dataset.iloc[:, -1].values
 
+
 # Encoding labels
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
+
 
 # Data processing
 X = np.zeros((1703, 10))
@@ -28,9 +31,11 @@ for i in range(1703):
     for j in range(10):
         X[i, j] = np.mean(X_raw[i, j])
 
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
 
 ## Feature scaling
 #from sklearn.preprocessing import StandardScaler
@@ -38,13 +43,34 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
 #X_train = sc_X.fit_transform(X_train)
 #X_test = sc_X.transform(X_test) # object already scaled on same basis
 
+
 # Creating and fitting classifier to the Training set
+
+# KNN
 from sklearn.neighbors import KNeighborsClassifier
-classifier = KNeighborsClassifier(5, p=2)
-classifier.fit(X_train, y_train) 
+knn = KNeighborsClassifier(5, p=2)
+knn.fit(X_train, y_train) 
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression 
+lr = LogisticRegression(solver='lbfgs', multi_class='multinomial')
+#lr.fit(X_train, y_train) 
+
+# SVM 
+from sklearn.svm import SVC 
+svm = SVC(kernel = 'linear', C = 1)
+svm.fit(X_train, y_train) 
+
+# Descision Tree
+from sklearn.tree import DecisionTreeClassifier 
+dtree = DecisionTreeClassifier(max_depth = 2)
+dtree.fit(X_train, y_train)
+
 
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+# Change classifier object
+y_pred = dtree.predict(X_test)
+
 
 # Testing accuracy
 from sklearn.metrics import confusion_matrix, accuracy_score
@@ -54,6 +80,4 @@ print('Misclassified samples: {}'.format(count_misclassified))
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy: {:.2f}'.format(accuracy))
 
-# Visualising the Training set results
 
-# Visualising the Test set results
