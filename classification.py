7c0d8c0f95b4fb75248f656a857ff0b7774118e2
Mederic <carriatmederic@gmail.com>
2019-01-22 15:14:09 +0200

    Happy birthday Cyril
    Signed-off-by: Mederic <carriatmederic@gmail.com>

--- classification.py
+++ classification.py
@@ -5,28 +5,28 @@ Created on Mon Jan 21 18:02:59 2019
 @author: Médéric Carriat
 """
 
-# Classification
+### Classification
 
-# Importing the libraries
+## Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# Importing the dataset
+## Importing the dataset
 dataset = pd.read_csv('dataset/y_train_final_kaggle.csv')
 X_raw = np.load("dataset/X_train_kaggle.npy")
 X_kaggle_raw = np.load("dataset/X_test_kaggle.npy")
 y = dataset.iloc[:, -1].values
 
 
-# Encoding labels
+## Encoding labels
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 
 
-# Data processing
+## Data processing
 X = np.zeros((1703, 10))
 for i in range(1703):
     for j in range(10):
@@ -38,19 +38,24 @@ for i in range(1705):
         X_kaggle[i , j] = np.mean(X_kaggle_raw[i, j])
 
 
-# Splitting the dataset into the Training set and Test set
+## Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
+
+# On split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+
+# On full data
+#X_train, y_train = X, y
 
 
 ## Feature scaling
-#from sklearn.preprocessing import StandardScaler
-#sc_X = StandardScaler()
-#X_train = sc_X.fit_transform(X_train)
-#X_test = sc_X.transform(X_test) # object already scaled on same basis
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test) # object already scaled on same basis
 
 
-# Creating and fitting classifier to the Training set
+## Creating and fitting classifier to the Training set
 
 # KNN
 from sklearn.neighbors import KNeighborsClassifier
@@ -67,7 +72,7 @@ from sklearn.svm import SVC
 svm = SVC(kernel = 'linear', C = 1)
 #svm.fit(X_train, y_train) 
 
-# Descision Tree
+# Decision Tree
 from sklearn.tree import DecisionTreeClassifier 
 dtree = DecisionTreeClassifier()
 dtree.fit(X_train, y_train)
@@ -78,13 +83,13 @@ gnb = GaussianNB()
 gnb.fit(X_train, y_train) 
   
 
-# Predicting the Test set results
+## Predicting the Test set results
 # Change classifier object
 y_pred = dtree.predict(X_test)
 y_kaggle = dtree.predict(X_kaggle)
 
 
-# Testing accuracy
+## Testing accuracy
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 count_misclassified = (y_test != y_pred).sum()
@@ -92,7 +97,8 @@ print('Misclassified samples: {}'.format(count_misclassified))
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy: {:.2f}'.format(accuracy))
 
-#Write .csv file
+
+## Write .csv file
 output = le.inverse_transform(y_kaggle)
 file = open("submission.csv", "w+")
 file.write("# Id,Surface\n")
