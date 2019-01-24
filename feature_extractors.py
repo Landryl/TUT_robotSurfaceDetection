3c50bb42dd1ef2bf2aa679cf07df9596aee4844d
Cyril Berger <cyril.berger@epita.fr>
2019-01-24 12:03:34 +0100

    Adding new extractor

--- feature_extractors.py
+++ feature_extractors.py
@@ -21,6 +21,17 @@ def deviationer(X_raw, size) :
             X[i, j+10] = np.std(X_raw[i, j])
     return X
 
+def deviationer_plus(X_raw, size) :
+    X = np.zeros((size, 40))
+    for i in range(size) :
+        for j in range(10) :
+            X[i, j] = np.mean(X_raw[i, j])
+            X[i, j+10] = np.std(X_raw[i, j])
+            X[i, j+20] = X_raw[i, j].max()
+            X[i, j+30] = X_raw[i, j].min()
+    return X
+
+
 def features_extractors_benchmark() :
     import loaders
     import tools
@@ -28,10 +39,11 @@ def features_extractors_benchmark() :
 
     extractors = [('raveller', raveller), 
                   ('averager', averager),
-                  ('deviationer', deviationer)]
+                  ('deviationer', deviationer),
+                  ('deviationer_plus', deviationer_plus)]
 
     for extractor in extractors :
-        X_train, y_train, X_test, y_test, le = loaders.load_for_train(0.20, extractor[1])
+        X_train, y_train, X_test, y_test, le = loaders.load_for_train_groups(0.20, extractor[1])
         lda = LinearDiscriminantAnalysis()
         lda.fit(X_train, y_train)
         y_pred = lda.predict(X_test)
