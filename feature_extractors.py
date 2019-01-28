0b2ccdbd53180865be2cb337fb9c56e86db7e77e
Landryl <Landryl@users.noreply.github.com>
2019-01-28 17:51:57 +0100

    Adding a new extractor taking in consideration the monotony of the signal

--- feature_extractors.py
+++ feature_extractors.py
@@ -1,4 +1,40 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+# Usefull functions
+
+def is_croissant(array, treshold) :
+    minimum_tolerated = array[0] - treshold
+    local = array[0]
+    i = 1
+    while local > minimum_tolerated and i < len(array) :
+        if (local - minimum_tolerated) > treshold :
+            minimum_tolerated = local - treshold
+        local = array[i]
+        i+=1
+    return 1 if i == len(array) else 0
+
+def is_decroissant(array, treshold) :
+    maximum_tolerated = array[0] + treshold
+    local = array[0]
+    i = 1
+    while local < maximum_tolerated and i < len(array) :
+        if (maximum_tolerated - local) > treshold :
+            maximum_tolerated = local + treshold
+        local = array[i]
+        i+=1
+    return 1 if i == len(array) else 0
+
+def is_monotonous(array, treshold) :
+    #print( 1 if is_decroissant(array, treshold) or is_croissant(array, treshold) else 0 )
+    #plt.plot(array)
+    #plt.show()
+    return 1 if is_decroissant(array, treshold) or is_croissant(array, treshold) else 0
+
+        
+
+# Extractors
 
 def raveller(X_raw, size) :
     X = np.zeros((size, 1280))
@@ -31,11 +67,24 @@ def deviationer_plus(X_raw, size) :
             X[i, j+30] = X_raw[i, j].min()
     return X
 
+def deviationer_monotonous(X_raw, size) :
+    X = np.zeros((size, 50))
+    for i in range(size) :
+        for j in range(10) :
+            X[i, j] = np.mean(X_raw[i, j])
+            X[i, j+10] = np.std(X_raw[i, j])
+            X[i, j+20] = X_raw[i, j].max()
+            X[i, j+30] = X_raw[i, j].min()
+            treshold = ( X_raw[i, j].max() - X_raw[i, j].min() ) / 4
+            X[i, j+40] = is_monotonous(X_raw[i, j], treshold)
+    return X
+
 def get_all_extractors() :
     return [('raveller', raveller),
             ('averager', averager),
             ('deviationer', deviationer),
-            ('deviationer_plus', deviationer_plus)]
+            ('deviationer_plus', deviationer_plus),
+            ('deviationer_monotonous', deviationer_monotonous)]
 
 def features_extractors_benchmark() :
     import loaders
@@ -45,7 +94,8 @@ def features_extractors_benchmark() :
     extractors = [('raveller', raveller), 
                   ('averager', averager),
                   ('deviationer', deviationer),
-                  ('deviationer_plus', deviationer_plus)]
+                  ('deviationer_plus', deviationer_plus),
+                  ('deviationer_monotonous', deviationer_monotonous)]
 
     for extractor in extractors :
         X_train, y_train, X_test, y_test, le = loaders.load_for_train(0.20, extractor[1])
