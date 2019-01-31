b42f4e9dcff7c80a5aca26d03e97cd6d404777af
Landryl <Landryl@users.noreply.github.com>
2019-01-31 17:15:26 +0100

    Adding local maximum counters (it is useless)

--- feature_extractors.py
+++ feature_extractors.py
@@ -33,7 +33,15 @@ def is_monotonous(array, treshold) :
     #plt.show()
     return 1 if is_decroissant(array, treshold) or is_croissant(array, treshold) else 0
 
-        
+def count_local_maximum(array) :
+#    plt.plot(array)
+    c = 0
+    for i in range(1, len(array) - 1) :
+        if ((array[i] - array[i - 1]) * (array[i + 1] - array[i]) < 0) :
+            c += 1
+#    print(c)
+#    plt.show()
+    return c
 
 # Extractors
 
@@ -59,13 +67,14 @@ def deviationer(X_raw, size) :
     return X
 
 def deviationer_plus(X_raw, size) :
-    X = np.zeros((size, 40))
+    X = np.zeros((size, 50))
     for i in range(size) :
         for j in range(10) :
             X[i, j] = np.mean(X_raw[i, j])
             X[i, j+10] = np.std(X_raw[i, j])
             X[i, j+20] = X_raw[i, j].max()
             X[i, j+30] = X_raw[i, j].min()
+            X[i, j+40] = count_local_maximum(tools.convolution_smooth(X_raw[i, j]))
     return X
 
 def deviationer_monotonous(X_raw, size) :
@@ -96,12 +105,12 @@ def euler_angles(X_raw, size):
 #            X[i, j+27] = X_raw[i, j].min()
 #    return X
     X_quater = deviationer_plus(X_raw, size)
-    X = np.zeros((size, 39))
+    X = np.zeros((size, 49))
     for i in range(size): 
         euler = tools.quaternionToEulerAngles(X_quater[i][0], X_quater[i][1], X_quater[i][2], X_quater[i][3])
         for j in range(3):
             X[i][j] = euler[j]
-        for j in range(3, 39):
+        for j in range(3, 49):
             X[i][j] = X_quater[i][j + 1]
     return X
 
