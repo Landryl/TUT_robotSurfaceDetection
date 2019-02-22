0a3ab72287e87110206409f7faa21982887843bd
Mederic <carriatmederic@gmail.com>
2019-02-22 15:30:39 +0200

    Add new file for features extraction
    Signed-off-by: Mederic <carriatmederic@gmail.com>

---
+++ classifiers/utilities/features_extractors2.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+X_raw = np.load("../dataset/X_train_kaggle.npy")
+features_labels = [] # to remember to add them 
+
+def features_extraction(X_raw):
+    X = [[]]*len(X_raw)
+    for i in range(len(X)):
+        features = []
+        # Mean
+        for j in range(10):
+            features.append(np.mean(X_raw[i, j]))
+        # Variance
+        for j in range(10):
+            features.append(np.var(X_raw[i, j]))
+        X[i] = features
+    return np.array(X)
+
+
+
+X = features_extraction(X_raw)
